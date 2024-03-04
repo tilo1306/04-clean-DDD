0ac0e70713a1a0561878ec0c85f3a130e9d5b294
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-03-04 15:23:43 -0300

    feat: Create entities question

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,16 @@
+import { randomUUID } from "node:crypto"
+
+ export class Question {
+
+  public id: string
+  public title: string
+  public content:string
+
+  constructor (title:string, content : string, id?:string){
+
+    this.id = id ?? randomUUID()
+    this.title = title
+    this.content = content
+
+  }
+}

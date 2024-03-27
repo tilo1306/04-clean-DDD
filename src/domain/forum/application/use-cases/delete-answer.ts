400a4cf9143fba26ca620740b2dd3fb44b914446
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-03-27 18:51:12 -0300

    feat: delete answer usecase

---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,31 @@
+import { IAnswersRepository } from '../repositories/IAnswersRepository'
+
+interface DeleteAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+interface DeleteAnswerUseCaseResponse {}
+
+export class DeleteAnswerUseCase {
+  constructor(private answersRepository: IAnswersRepository) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answersRepository.delete(answer)
+
+    return {}
+  }
+}

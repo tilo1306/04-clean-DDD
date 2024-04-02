39f2d0d004fb4d490db8b33adc5103715fe212e6
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-04-02 16:35:09 -0300

    test> List question comments

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -1,7 +1,7 @@
 import { makeAnswer } from 'test/factories/make-answer'
-import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 import { InMemoryAnswerRepository } from '@/test/repositories/in-memories-answer-repository'
+import { InMemoryAnswerCommentsRepository } from '@/test/repositories/in-memory-answer-comments-repository'
 
 let inMemoryAnswersRepository: InMemoryAnswerRepository
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
@@ -29,7 +29,7 @@ describe('Comment on Answer', () => {
       content: 'Comentário teste',
     })
 
-    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+    expect(inMemoryAnswerCommentsRepository.db[0].content).toEqual(
       'Comentário teste',
     )
   })

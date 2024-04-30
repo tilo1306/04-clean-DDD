3df55201a24a4df6cc478511ae68829bb6570441
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-04-30 13:00:44 -0300

    feat: events

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { UniqueEntityID } from './unique-entity-id'
 
 export abstract class Entity<Props> {
@@ -9,7 +10,19 @@ export abstract class Entity<Props> {
   }
 
   protected constructor(props: Props, id?: UniqueEntityID) {
-    this._id = id ?? new UniqueEntityID(id)
     this.props = props
+    this._id = id ?? new UniqueEntityID()
+  }
+
+  public equals(entity: Entity<any>) {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
   }
 }
